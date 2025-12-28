749b997ca3845104d9f21ad609afaa8a4a303e38
brunobenja <116063204+brunobenja@users.noreply.github.com>
2025-12-28 12:55:30 +0100

    lv2 contacts

---
+++ contact-book-api/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const contactRoutes = require("./routes/contacts");
+const path = require('path');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Middleware
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+// Routes
+app.use("/contacts", contactRoutes);
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
